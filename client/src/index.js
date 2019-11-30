458582915bc670629637d4824c06dd46e8887cf9
kevin <kevinchuwinghim@gmail.com>
2019-12-01 02:29:47 +0800

    Add panel layout

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "normalize.css";
+import "@blueprintjs/core/lib/css/blueprint.css"; 
+import "@blueprintjs/icons/lib/css/blueprint-icons.css";
+import "./css/index.css";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import rootReducer from "./reducers";
+const store = createStore(rootReducer);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>, 
+document.getElementById("root"));
